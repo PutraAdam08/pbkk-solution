16a3bc73284512265042f7e2688ad6e1b38962ab
PutraAdam08 <putraadam322@gmail.com>
2024-09-11 13:44:23 +0700

    Fix routing and views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:title>
+        home
+    </x-slot>
+    <div class="container mx-auto py-6">
+        <div class="flex flex-wrap ms-36 my-56  justify-start max-w-4xl">
+            <h1 class="flex-none w-98 text-white text-8xl">HELLO WORLD!!</h1>
+            <div class="flex items-end justify-start">
+                <h2 class="ms-2 flex-none text-xl text-white">this web is my take on framework programming assigntment.<br> I will add more pages in the future.</h2>
+            </div>
+        </div>
+    </div>
+</x-layout>
